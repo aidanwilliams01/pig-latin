8bd9896da98714ba6d64b083fc4ba71a48c34a3d
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-11 14:45:11 -0700

    fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function pigLatin(text) {
   if (text[0].toLowerCase().includes("a") || text[0].toLowerCase().includes("e") || text[0].toLowerCase().includes("i") || text[0].toLowerCase().includes("o") || text[0].toLowerCase().includes("u"))  {
-      return text.concat("way");
+    return text.concat("way");
   }
   else if (text.slice(0, 2).toLowerCase() === "qu") {
-      return text.slice(2).concat(text.slice(0, 2)).concat("ay")
-    }
+    return text.slice(2).concat(text.slice(0, 2)).concat("ay")
+  }
   else {
-      for (let index = 1; index < text.length; index++) {
-          if (text[index].toLowerCase().includes("a") || text[index].toLowerCase().includes("e") || text[index].toLowerCase().includes("i") || text[index].toLowerCase().includes("o") || text[index].toLowerCase().includes("u"))  {
-          return text.slice(index).concat(text.slice(0, index)).concat("ay")
-          }
+    for (let index = 1; index < text.length; index++) {
+      if (text[index].toLowerCase().includes("a") || text[index].toLowerCase().includes("e") || text[index].toLowerCase().includes("i") || text[index].toLowerCase().includes("o") || text[index].toLowerCase().includes("u"))  {
+        return text.slice(index).concat(text.slice(0, index)).concat("ay")
       }
+    }
   }
 }
