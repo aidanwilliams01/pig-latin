9934d94c766192733c69fc5360e2ef1c34cba9fc
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-11 15:26:35 -0700

    add semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function pigLatin(text) {
   const textArray = text.split(" ");
-  let results = ""
+  let results = "";
   textArray.forEach(function(word) {
     if (word[0].toLowerCase().includes("a") || word[0].toLowerCase().includes("e") || word[0].toLowerCase().includes("i") || word[0].toLowerCase().includes("o") || word[0].toLowerCase().includes("u"))  {
       results = results.concat(" ").concat(word.concat("way"));
@@ -11,10 +11,10 @@ function pigLatin(text) {
     else {
       for (let index = 1; index < text.length; index++) {
         if (word[index].toLowerCase().includes("a") || word[index].toLowerCase().includes("e") || word[index].toLowerCase().includes("i") || word[index].toLowerCase().includes("o") || word[index].toLowerCase().includes("u"))  {
-          results = results.concat(" ").concat(word.slice(index).concat(word.slice(0, index)).concat("ay"))
+          results = results.concat(" ").concat(word.slice(index).concat(word.slice(0, index)).concat("ay"));
           return results;
         }
       }
     }});
-  return results.trim()
+  return results.trim();
 }
