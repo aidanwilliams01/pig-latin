be6f058a6cb5d6c87f48f92b3d46de37aa2ea406
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-11 15:25:58 -0700

    update pigLatin function to transate input with multiple words

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 function pigLatin(text) {
-  if (text[0].toLowerCase().includes("a") || text[0].toLowerCase().includes("e") || text[0].toLowerCase().includes("i") || text[0].toLowerCase().includes("o") || text[0].toLowerCase().includes("u"))  {
-    return text.concat("way");
-  }
-  else if (text.slice(0, 2).toLowerCase() === "qu") {
-    return text.slice(2).concat(text.slice(0, 2)).concat("ay")
-  }
-  else {
-    for (let index = 1; index < text.length; index++) {
-      if (text[index].toLowerCase().includes("a") || text[index].toLowerCase().includes("e") || text[index].toLowerCase().includes("i") || text[index].toLowerCase().includes("o") || text[index].toLowerCase().includes("u"))  {
-        return text.slice(index).concat(text.slice(0, index)).concat("ay")
-      }
+  const textArray = text.split(" ");
+  let results = ""
+  textArray.forEach(function(word) {
+    if (word[0].toLowerCase().includes("a") || word[0].toLowerCase().includes("e") || word[0].toLowerCase().includes("i") || word[0].toLowerCase().includes("o") || word[0].toLowerCase().includes("u"))  {
+      results = results.concat(" ").concat(word.concat("way"));
     }
-  }
+    else if (word.slice(0, 2).toLowerCase() === "qu") {
+      results = results.concat(" ").concat(word.slice(2).concat(word.slice(0, 2)).concat("ay"));
+    }
+    else {
+      for (let index = 1; index < text.length; index++) {
+        if (word[index].toLowerCase().includes("a") || word[index].toLowerCase().includes("e") || word[index].toLowerCase().includes("i") || word[index].toLowerCase().includes("o") || word[index].toLowerCase().includes("u"))  {
+          results = results.concat(" ").concat(word.slice(index).concat(word.slice(0, index)).concat("ay"))
+          return results;
+        }
+      }
+    }});
+  return results.trim()
 }
